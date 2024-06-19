74667e5be889dbefcfb2df2b048793a30916a1ca
Jocelyn <jocelynmg@hotmail.com>
2024-06-19 23:01:17 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md','r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = 'nlp_preprocess_jocelyn',
+    version = '0.0.1',
+    author = 'Jocelyn Teh',
+    description= 'This is Preprocessing Package NLP',
+    long_description= long_description,
+    long_description_content_type= 'text/markdown',
+    packages= setuptools.find_packages(),
+    classifiers= [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.5'
+)
